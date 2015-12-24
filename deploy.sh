93f1c676b93f7071c4ea25cb15ddd139abfd4822
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-12-23 18:56:52 -0800

    Removed the _deploy submodule, updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 if ghc --make site.hs; then
     ./site clean
     ./site build
-    cp -R _site/* _deploy/
-    cd _deploy
+    cp -R _site/* ../beloglazov.github.com/
+    cd ../beloglazov.github.com
     git add .
     git commit -a -m "Deployment on `date`"
     git push -u origin master
